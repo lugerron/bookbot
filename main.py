8d9b3e5dea40f608a1d3ea8861b9c4ee79c3364e
lugerron <lugerron@burnedwoods.com>
2024-09-26 15:08:50 -0400

    Feature complete

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ def main():
     num_words = count_words(file_contents)
     dictionary = count_characters(file_contents)
 
-    print(num_words)
-    print(dictionary)
+    print_report(num_words, dictionary, "books/frankenstein.txt")
 
 def count_words(text):
     words = text.split()
@@ -20,11 +19,35 @@ def count_characters(text):
     for word in words:
         lowered_word = word.lower()
         for char in lowered_word:
-            if char not in char_dictionary:
+            if char not in char_dictionary and char.isalpha():
                 char_dictionary[char] = 1
-            else:
+            elif char.isalpha():
                 char_dictionary[char] += 1
-    
+    #TODO: Convert your dictionary of characters into a list of dictionaries and then use the .sort() method to sort by the number of occurrences.
     return char_dictionary
+
+def dict_to_list(dict_to_convert):
+    dict_list = []
+    for value in dict_to_convert:
+        tmp_dict = {}
+        tmp_dict["name"] = value
+        tmp_dict["num"] = dict_to_convert[value]
+        dict_list.append(tmp_dict)
+    return dict_list
+
+def sort_on(dict_to_sort):
+    return dict_to_sort["num"]
+
+def print_report(word_count, word_dict, book_name):
+    list_to_print = dict_to_list(word_dict)
+    list_to_print.sort(reverse=True, key=sort_on)
+    print(f"--- Begin report of {book_name} ---")
+    print(f"{word_count} words found in the document.")
+    print("")
+    
+    for list_dict in list_to_print:
+        print(f"The '{list_dict['name']}' character was found {list_dict['num']} times.")
+    print("--- End report ---")
+    return None
 
 main()
